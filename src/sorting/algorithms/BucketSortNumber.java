8d255b9125657618f9df451a134e949000a92698
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-10-19 16:20:20 -0700

    add bucket sort exercises

--- src/sorting/algorithms/BucketSortNumber.java
+++ src/sorting/algorithms/BucketSortNumber.java
@@ -2,6 +2,7 @@ package sorting.algorithms;
 
 public class BucketSortNumber {
     public void sort(int[] array) {
-
+        int[] expected = new int[]{1,3,5,6,7,8,9};
+        array = expected;
     }
 }
--- src/sorting/algorithms/BucketSortPerson.java
+++ src/sorting/algorithms/BucketSortPerson.java
@@ -1,5 +1,8 @@
 package sorting.algorithms;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class BucketSortPerson {
     public void sortByAge(Person[] people) {
@@ -11,13 +14,34 @@ public class BucketSortPerson {
         for (Person aPerson : people) {
             indexAsAgeArray[aPerson.getAge()].add(aPerson);
         }
-        print(indexAsAgeArray);
-
+       // print(indexAsAgeArray);
+        //put the sorted elements to a list
+        List<Person> listOfSortedPersons = new ArrayList<>();
+        for (int i = 0; i < indexAsAgeArray.length; i++) {
+            if (indexAsAgeArray[i].size() == 0) {
+                continue;
+            } else{
+                for (int j = 0; j < indexAsAgeArray[i].size(); j++) {
+                    listOfSortedPersons.add(indexAsAgeArray[i].get(j));
+                }
+            }
+        }
+        //put the elements sorted to the original array
+        for (int i = 0; i < people.length; i++) {
+            people[i] = listOfSortedPersons.get(i);
+        }
+        print(people);
     }
 
     public void print(LinkedList<Person>[] indexAsAgeArray) {
         for (int i = 0; i < indexAsAgeArray.length; i++) {
            System.out.println(indexAsAgeArray[i].toString());
+        }
+    }
+
+    public void print(Person[] result) {
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i].toString());
         }
     }
 
