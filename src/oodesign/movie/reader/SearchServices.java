0d7087accfe0d8bac99758c04903c3a586417152
SerenaPang <ypang4@usfca.edu>
2021-10-27 10:45:45 -0700

    Search functions completed

--- src/oodesign/movie/reader/SearchServices.java
+++ src/oodesign/movie/reader/SearchServices.java
@@ -18,23 +18,58 @@ public class SearchServices {
         this.mapOfMovies = mapForMovies;
     }
 
+    public static void printMap(HashMap<Integer, Movie> mapOfMovies) {
+        System.out.println("Printing map: ");
+        for (Integer aMovie : mapOfMovies.keySet()) {
+            System.out.println("ID: " + mapOfMovies.get(aMovie).ID
+                    + " Name: " + mapOfMovies.get(aMovie).NAME + " Year: " + mapOfMovies.get(aMovie).YEAR);
+        }
+    }
 
-    public List<Movie> searchByID(int id) {
-        List<Movie> result = new ArrayList<>();
-
+    /**
+     * return a list of result movie ids with the search by id
+     */
+    public List<Integer> searchByID(int id) {
+        List<Integer> result = new ArrayList<>();
+        if (mapOfMovies.containsKey(id)) {
+            result.add(id);
+        }
         return result;
     }
 
-    public List<Movie> searchByName(String name) {
-        List<Movie> result = new ArrayList<>();
-
+    /**
+     * return a list of result movie ids with the search by name
+     */
+    public List<Integer> searchByName(String name) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer aMovie : mapOfMovies.keySet()) {
+            if (mapOfMovies.get(aMovie).NAME.equals(name)) {
+                result.add(mapOfMovies.get(aMovie).ID);
+            }
+        }
         return result;
     }
 
-    public List<Movie> searchByYear(String name) {
-        List<Movie> result = new ArrayList<>();
-
+    /**
+     * return a list of result movie ids with the search by year
+     */
+    public List<Integer> searchByYear(String year) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer aMovie : mapOfMovies.keySet()) {
+            if (mapOfMovies.get(aMovie).YEAR.equals(year)) {
+                result.add(mapOfMovies.get(aMovie).ID);
+            }
+        }
         return result;
+    }
+
+    /**
+     * prints the result movies including the id, name, and year
+     */
+    public void printSearchResults(List<Integer> result) {
+        for (Integer id : result) {
+            System.out.println("ID: " + id + " Name: " + mapOfMovies.get(id).NAME + " Year: " + mapOfMovies.get(id).YEAR);
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -44,7 +79,15 @@ public class SearchServices {
         // print(movies);
         MovieMap mm = new MovieMap();
         HashMap<Integer, Movie> mapOfMovies = mm.buildIdAndMovieMap(movies);
-        printMap(mapOfMovies);
+        // printMap(mapOfMovies);
+        SearchServices ss = new SearchServices(mapOfMovies);
+        //test search by id
+        ss.printSearchResults(ss.searchByID(15)); //ID: 15 Name: Casablanca Year: 1942
+        //test search by name
+        ss.printSearchResults(ss.searchByName("The Godfather"));//ID: 25 Name: The Godfather Year: 1972
+        //test search by year
+        ss.printSearchResults(ss.searchByYear("1919"));//ID: 27 Name: Broken Blossoms Year: 1919
+        ss.printSearchResults(ss.searchByYear("1944"));
     }
 
 }
