b88fa606adb414176f59a9b4da58126eff025982
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-10-30 12:36:14 -0700

    oo design merge k function compeleted

--- src/oodesign/mergek/MyFileReaderSolutionTwo.java
+++ src/oodesign/mergek/MyFileReaderSolutionTwo.java
@@ -5,73 +5,204 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class reads the input files and merge them into one file
- * */
+ */
 public class MyFileReaderSolutionTwo {
+    /**
+     * This function compares the line of the two files and output the smaller then the large one to the output file
+     */
+    public void mergeFiles(File firstInputFile, File secondInputFile, Path outputPath) throws IOException {
+        MyFileWriter writer = new MyFileWriter();
+
+        FileReader firstFR = new FileReader(firstInputFile);
+        BufferedReader br = new BufferedReader(firstFR);
+
+        FileReader secondFR = new FileReader(secondInputFile);
+        BufferedReader brSecond = new BufferedReader(secondFR);
+
+        System.out.println("Reading " + firstInputFile.getName().toString() + " and  " + secondInputFile.getName().toString() + "... ...");
+
+        String firstLine;
+        String secondLine;
+        List<String> listOfSortedResult = new ArrayList<>();
+        while ((firstLine = br.readLine()) != null && (secondLine = brSecond.readLine()) != null) {
+
+            //for each line in the first file, we extract the id, name and year
+            String[] messageArrayFirst = firstLine.split(":");
+            String timeStrFirst = messageArrayFirst[0];
+            int timeFirst = Integer.parseInt(timeStrFirst);
+            String messageFirst = messageArrayFirst[1];
+
+            //for each line in the second file, we extract the id, name and year
+            String[] messageArraySecond = secondLine.split(":");
+            String timeStrSecond = messageArraySecond[0];
+            int timeSecond = Integer.parseInt(timeStrSecond);
+            String messageSecond = messageArraySecond[1];
+
+            //compare the time of the two lines in the file, write the lines to the output file
+
+            if (timeFirst <= timeSecond) {
+                listOfSortedResult.add(firstLine);
+                //writer.writeLine(outputPath, firstLine);
+            } else {
+                listOfSortedResult.add(secondLine);
+                // writer.writeLine(outputPath, secondLine);
+            }
+        }
         /**
-         * This function compares the line of the two files and output the smaller then the large one to the output file
-         * */
-        public void mergeFiles(File firstInputFile, File secondInputFile, Path outputPath) throws IOException {
-            MyFileWriter writer = new MyFileWriter();
+         //if file one finished first, then put everything left on file two to the output file
+         if ((firstLine = br.readLine()) != null) {
+         while ((firstLine = br.readLine()) != null) {
+         writer.writeLine(outputPath, firstLine);
+         }
+         }
 
-            FileReader firstFR = new FileReader(firstInputFile);
-            BufferedReader br = new BufferedReader(firstFR);
+         //if file two finished first, put everything left on file one to the output file
+         if ((secondLine = brSecond.readLine()) != null) {
+         while ((secondLine = brSecond.readLine()) != null) {
+         writer.writeLine(outputPath, secondLine);
+         }
+         }
+         */
+        print(listOfSortedResult);
+    }
 
-            FileReader secondFR = new FileReader(secondInputFile);
-            BufferedReader brSecond = new BufferedReader(secondFR);
 
-            System.out.println("Reading " + firstInputFile.getName().toString() + " and  " + secondInputFile.getName().toString() + "... ...");
+    public void merge(File firstInputFile, File secondInputFile, Path outputPath) throws IOException {
+        MyFileWriter writer = new MyFileWriter();
 
-            String firstLine;
-            String secondLine;
-            while ((firstLine = br.readLine()) != null && (secondLine = brSecond.readLine()) != null) {
+        FileReader firstFR = new FileReader(firstInputFile);
+        BufferedReader br = new BufferedReader(firstFR);
 
-                //for each line in the first file, we extract the id, name and year
-                String[] messageArrayFirst = firstLine.split(":");
-                String timeStrFirst = messageArrayFirst[0];
-                int timeFirst = Integer.parseInt(timeStrFirst);
-                String messageFirst = messageArrayFirst[1];
+        FileReader secondFR = new FileReader(secondInputFile);
+        BufferedReader brSecond = new BufferedReader(secondFR);
 
-                //for each line in the second file, we extract the id, name and year
-                String[] messageArraySecond = secondLine.split(":");
-                String timeStrSecond = messageArraySecond[0];
-                int timeSecond = Integer.parseInt(timeStrSecond);
-                String messageSecond = messageArraySecond[1];
+        System.out.println("Reading " + firstInputFile.getName().toString() + " and  " + secondInputFile.getName().toString() + "... ...");
 
-                //compare the time of the two lines in the file, write the lines to the output file
+        String firstLine = br.readLine();
+        String secondLine = brSecond.readLine();
+        List<String> listOfSortedResult = new ArrayList<>();
 
-                if (timeFirst <= timeSecond) {
-                    writer.writeLine(outputPath, firstLine);
-                } else {
-                    writer.writeLine(outputPath, secondLine);
-                }
-            }
-            //if file one finished first, then put everything left on file two to the output file
-            if ((firstLine = br.readLine()) != null) {
-                while ((firstLine = br.readLine()) != null) {
-                    writer.writeLine(outputPath, firstLine);
-                }
-            }
+        while (firstLine != null && secondLine != null) {
 
-            //if file two finished first, put everything left on file one to the output file
-            if ((secondLine = brSecond.readLine()) != null) {
-                while ((secondLine = brSecond.readLine()) != null) {
-                    writer.writeLine(outputPath, secondLine);
-                }
+            //for each line in the first file, we extract the id, name and year
+            String[] messageArrayFirst = firstLine.split(":");
+            String timeStrFirst = messageArrayFirst[0];
+            int timeFirst = Integer.parseInt(timeStrFirst);
+            String messageFirst = messageArrayFirst[1];
+
+            //for each line in the second file, we extract the id, name and year
+            String[] messageArraySecond = secondLine.split(":");
+            String timeStrSecond = messageArraySecond[0];
+            int timeSecond = Integer.parseInt(timeStrSecond);
+            String messageSecond = messageArraySecond[1];
+
+            //compare the time of the two lines in the file, write the lines to the output file
+
+            if (timeFirst <= timeSecond) {
+                listOfSortedResult.add(firstLine);
+                writer.writeLine(outputPath, firstLine);
+                firstLine = br.readLine();
+            } else {
+                listOfSortedResult.add(secondLine);
+                writer.writeLine(outputPath, secondLine);
+                secondLine = brSecond.readLine();
             }
         }
 
-        public static void main(String[] args) throws IOException {
-           MyFileReaderSolutionTwo mfr = new MyFileReaderSolutionTwo();
+        //if file one finished first, then put everything left on file two to the output file
+        //be careful about the sequence of putting the current line first and then iterate to the next line
+            while (firstLine != null) {
+                listOfSortedResult.add(firstLine);
+                writer.writeLine(outputPath, firstLine);
+                firstLine = br.readLine();
+            }
 
-            File filex = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filex");
-            File filey = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filey");
-            File filez = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filez");
 
-            //write files to the output location
-            File output = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/output");
-            mfr.mergeFiles(filex, filey, output.toPath());
+        //if file two finished first, put everything left on file one to the output file
+            while (secondLine != null) {
+                listOfSortedResult.add(secondLine);
+                writer.writeLine(outputPath, secondLine);
+                secondLine = brSecond.readLine();
+            }
+        print(listOfSortedResult);
+    }
+
+    public List<String> merge(File firstInputFile, File secondInputFile) throws IOException {
+        MyFileWriter writer = new MyFileWriter();
+
+        FileReader firstFR = new FileReader(firstInputFile);
+        BufferedReader br = new BufferedReader(firstFR);
+
+        FileReader secondFR = new FileReader(secondInputFile);
+        BufferedReader brSecond = new BufferedReader(secondFR);
+
+        System.out.println("Reading " + firstInputFile.getName().toString() + " and  " + secondInputFile.getName().toString() + "... ...");
+
+        String firstLine = br.readLine();
+        String secondLine = brSecond.readLine();
+        List<String> listOfSortedResult = new ArrayList<>();
+
+        while (firstLine != null && secondLine != null) {
+
+            //for each line in the first file, we extract the id, name and year
+            String[] messageArrayFirst = firstLine.split(":");
+            String timeStrFirst = messageArrayFirst[0];
+            int timeFirst = Integer.parseInt(timeStrFirst);
+            String messageFirst = messageArrayFirst[1];
+
+            //for each line in the second file, we extract the id, name and year
+            String[] messageArraySecond = secondLine.split(":");
+            String timeStrSecond = messageArraySecond[0];
+            int timeSecond = Integer.parseInt(timeStrSecond);
+            String messageSecond = messageArraySecond[1];
+
+            //compare the time of the two lines in the file, write the lines to the output file
+
+            if (timeFirst <= timeSecond) {
+                listOfSortedResult.add(firstLine);
+                firstLine = br.readLine();
+            } else {
+                listOfSortedResult.add(secondLine);
+                secondLine = brSecond.readLine();
+            }
         }
+
+        //if file one finished first, then put everything left on file two to the output file
+        //be careful about the sequence of putting the current line first and then iterate to the next line
+        while (firstLine != null) {
+            listOfSortedResult.add(firstLine);
+            firstLine = br.readLine();
+        }
+
+
+        //if file two finished first, put everything left on file one to the output file
+        while (secondLine != null) {
+            listOfSortedResult.add(secondLine);
+            secondLine = brSecond.readLine();
+        }
+       return listOfSortedResult;
+    }
+
+    public void print(List<String> result) {
+        for (String line : result) {
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        MyFileReaderSolutionTwo mfr = new MyFileReaderSolutionTwo();
+
+        File filex = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filex");
+        File filey = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filey");
+        File filez = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filez");
+
+        //write files to the output location
+        File output = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/output");
+        mfr.merge(filex, filey, output.toPath());
+    }
 }
--- src/oodesign/mergek/MyFileWriter.java
+++ src/oodesign/mergek/MyFileWriter.java
@@ -1,6 +1,7 @@
 package oodesign.mergek;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -15,17 +16,24 @@ import java.util.List;
 public class MyFileWriter {
 
     //writing the merging result to the out put file
-    public void writeLine(Path path, HashMap<Integer, List<String>> result) throws IOException{
+    public void writeLine(Path path, HashMap<Integer, List<String>> result) throws IOException {
         String content = "hello there";
-
         Files.write(path, content.getBytes(StandardCharsets.UTF_8));
     }
 
-    public void writeLine(Path path, String content) throws IOException{
-        content = "hello there";
-
+    public void writeLine(Path path, String content) throws IOException {
         Files.write(path, content.getBytes(StandardCharsets.UTF_8));
     }
+
+    public void writeToFile(Path path ,List<String> result) throws IOException {
+        FileWriter writer = new FileWriter(path.toFile());
+        for (String aLine : result) {
+            //System.out.println(aLine);
+           writer.write(aLine + System.lineSeparator());
+        }
+        writer.close();
+    }
+
     public static void print(HashMap<Integer, List<String>> messageMap) {
         System.out.println("Printing map: ");
         for (Integer key : messageMap.keySet()) {
@@ -37,37 +45,18 @@ public class MyFileWriter {
     }
 
     public static void main(String[] args) throws IOException {
-        MyFileReader fr = new MyFileReader();
+        MyFileReaderSolutionTwo reader = new MyFileReaderSolutionTwo();
 
         File filex = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filex");
         File filey = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filey");
         File filez = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filez");
-
-        HashMap<Integer, List<String>> filexMapOfMessages = fr.extractMessageAndTime(filex);
-        //   print(filexMapOfMessages);
-
-        HashMap<Integer, List<String>> fileyMapOfMessages = fr.extractMessageAndTime(filey);
-        //  print(fileyMapOfMessages);
-
-        HashMap<Integer, List<String>> filezMapOfMessages = fr.extractMessageAndTime(filez);
-        //  print(filexMapOfMessages);
-
-        MergeKSortedMaps merge = new MergeKSortedMaps();
-        //HashMap<Integer, List<String>> result = merge.mergeTwoMaps(filexMapOfMessages, fileyMapOfMessages);
-        //add all maps we want to merge to the list
-        List<HashMap<Integer, List<String>>> mapsToBeMerged = merge.addMap(filexMapOfMessages);
-        mapsToBeMerged.add(fileyMapOfMessages);
-        mapsToBeMerged.add(filezMapOfMessages);
-
-        //start merging the maps
-        HashMap<Integer, List<String>> result = merge.mergeLisOfMaps(mapsToBeMerged);
-        print(result);
+        //merge result
+        List<String> result = reader.merge(filex, filey);
 
         //write files to the output location
-        File output = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/output");
+      //  File output = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/output");
+        Path out = Path.of("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/output");
         MyFileWriter writer = new MyFileWriter();
-        writer.writeLine(output.toPath(), result);
+        writer.writeToFile(out, result);
     }
-
-
 }
