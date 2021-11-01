33adc4fbb7940c095efbf66ed139dd06a99ca1e9
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-11-01 10:43:02 -0700

    oo design merge k DONE

--- src/oodesign/mergek/Driver.java
+++ src/oodesign/mergek/Driver.java
@@ -14,8 +14,15 @@ public class Driver {
     //This function takes in a list of files needs to be merged, and output the result to the output file
     public static void startMerging(List<File> filesToBeMerged, File output) throws IOException {
         MyFileReaderSolutionTwo reader = new MyFileReaderSolutionTwo();
-        for (File aFile : filesToBeMerged) {
-            reader.mergeFilesToAFile(aFile, output, output);
+        if (filesToBeMerged.size() <= 1) {
+            System.out.println("No files needs to be merged");
+            return;
+        }
+        //merge the first 2 files so that we have a file to be merged at start, it won't start copying the first file to the output, and then start the merge
+        reader.mergeFilesToAFile(filesToBeMerged.get(0), filesToBeMerged.get(1),output);
+        //if there are just 2 files to be merged, it won't enter to this loop, so it will merged 2 files to ouput, else itwill merge all the files
+        for (int i = 2; i < filesToBeMerged.size(); i++) {
+            reader.mergeFilesToAFile(filesToBeMerged.get(i), output, output);
         }
     }
 
@@ -25,13 +32,18 @@ public class Driver {
         File filey = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filey");
         File filez = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filez");
         File filerandom = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filerandom");
+        File filerupikaur = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filerupikaur");
+        File fileselflove = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/fileselflove");
 
         //group it to a list of files
         List<File> filesToBeMerged = new ArrayList<>();
         filesToBeMerged.add(filex);
         filesToBeMerged.add(filey);
+
         filesToBeMerged.add(filez);
         filesToBeMerged.add(filerandom);
+        filesToBeMerged.add(filerupikaur);
+        filesToBeMerged.add(fileselflove);
 
         //output file
         File fileoutput= new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/output");
