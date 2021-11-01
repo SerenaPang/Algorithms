0272871bfb299b5c8c7fed7335858ae4c62dca0c
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-11-01 10:14:00 -0700

    oo design merge k compeleted driver

--- src/oodesign/mergek/Driver.java
+++ src/oodesign/mergek/Driver.java
@@ -2,25 +2,39 @@ package oodesign.mergek;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class provides the file path, and the files needs to be merged.
  * and then writes the merge result to the output file
  * */
 public class Driver {
-    public static void main(String[] args) throws IOException {
-        MyFileReaderSolutionTwo reader = new MyFileReaderSolutionTwo();
 
+    //This function takes in a list of files needs to be merged, and output the result to the output file
+    public static void startMerging(List<File> filesToBeMerged, File output) throws IOException {
+        MyFileReaderSolutionTwo reader = new MyFileReaderSolutionTwo();
+        for (File aFile : filesToBeMerged) {
+            reader.mergeFilesToAFile(aFile, output, output);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        //input files to be merged
         File filex = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filex");
         File filey = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filey");
         File filez = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filez");
+        File filerandom = new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/filerandom");
 
-        //read file x and file y, output to file xy
-        File filexy= new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/outputxy");
-        reader.mergeFilesToAFile(filey,filex, filexy);
+        //group it to a list of files
+        List<File> filesToBeMerged = new ArrayList<>();
+        filesToBeMerged.add(filex);
+        filesToBeMerged.add(filey);
+        filesToBeMerged.add(filez);
+        filesToBeMerged.add(filerandom);
 
-        //read output file xy and file z, output it to file xyz
-        File filexyz= new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/outputxyz");
-        reader.mergeFilesToAFile(filexy,filez, filexyz);
+        //output file
+        File fileoutput= new File("/Users/serenapang/Development/Algorithms/src/oodesign/mergek/output");
+        startMerging(filesToBeMerged, fileoutput);
     }
 }
